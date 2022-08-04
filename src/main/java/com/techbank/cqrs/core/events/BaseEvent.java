103aaeb104bff01f91f0da15d0d75e9227743f41
Charly <charly81.zirpoli@gmail.com>
2022-08-04 12:55:47 -0300

    Finish AccountAggregates

--- src/main/java/com/techbank/cqrs/core/events/BaseEvent.java
+++ src/main/java/com/techbank/cqrs/core/events/BaseEvent.java
@@ -5,11 +5,12 @@ import com.techbank.cqrs.core.messages.Message;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@SuperBuilder
 public abstract class BaseEvent extends Message {
 	private int version;
-
 }
